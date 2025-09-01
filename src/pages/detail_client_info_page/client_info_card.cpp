794cb84187dd9e51fac391ca3a91d224b58c7080
Artem Durynin <mail@durynin1.ru>
2025-09-01 20:34:11 +0300

    feat: Add new signals and slots

--- src/pages/detail_client_info_page/client_info_card.cpp
+++ src/pages/detail_client_info_page/client_info_card.cpp
@@ -5,55 +5,113 @@ Q_LOGGING_CATEGORY(logClientInfoCard, "pcm.ClientInfoCard");
 
 QClientInfoCardPage::QClientInfoCardPage(QWidget *parent)
     : QWidget(parent), mUi(std::make_unique<Ui::ClientInfoCard>()) {
-  mUi->setupUi(this);
-}
+    mUi->setupUi(this);
 
-void QClientInfoCardPage::setClientInfo(
-    const std::optional<ObxClient> &client) {
-  if (client.has_value()) {
-    mClientInfo.update(client.value());
-  } else {
-    mClientInfo.clear();
-  }
+    connectReactiveSignals();
+    connectSignals();
 
-  emit clearUI();
-  emit provideUpdateUI();
-}
-void QClientInfoCardPage::updateUiProperty() {
-  QString initials;
-  if (!mClientInfo.getName().isEmpty())
-    initials.append(mClientInfo.getName().at(0).toUpper());
-  if (!mClientInfo.getLastName().isEmpty())
-    initials.append(mClientInfo.getLastName().at(0).toUpper());
-  mUi->avatarLabel->setText(initials.isEmpty() ? "??" : initials);
-
-  mUi->nameInput->setText(mClientInfo.getName());
-  mUi->lastNameInput->setText(mClientInfo.getLastName());
-  mUi->phoneInput->setText(mClientInfo.getPhoneNumber());
-  mUi->emailInput->setText(mClientInfo.getEmail());
-
-  mUi->birthdateInput->setDate(
-      mClientInfo.getBirthDate().isValid()
-          ? mClientInfo.getBirthDate()
-          : QDate::currentDate());
-  mUi->ageInput->setText(mClientInfo.getAge());
-
-  mUi->countryInput->setText(mClientInfo.getCountry());
-  mUi->cityInput->setText(mClientInfo.getCity());
-  mUi->timezoneInput->setText(mClientInfo.getTimezone());
-
-  mUi->diagnosisTextEdit->setPlainText(mClientInfo.getDiagnosis());
-  mUi->additionalInfoTextEdit->setPlainText(mClientInfo.getAdditionalInfo());
+    emit provideUpdateUI();
 }
 
 QClientInfoCardPage::~QClientInfoCardPage() = default;
 
+void QClientInfoCardPage::setClientInfo(const std::optional<ObxClient> &client) {
+    if (client.has_value()) {
+        mClientInfo.update(client.value());
+    } else {
+        mClientInfo.clear();
+    }
+
+    emit provideClearUI();
+    emit provideUpdateUI();
+}
+
+void QClientInfoCardPage::updateUiProperty() const {
+    QString initials;
+    if (!mClientInfo.getName().isEmpty())
+        initials.append(mClientInfo.getName().at(0).toUpper());
+    if (!mClientInfo.getLastName().isEmpty())
+        initials.append(mClientInfo.getLastName().at(0).toUpper());
+    mUi->avatarLabel->setText(initials.isEmpty() ? "??" : initials);
+
+    mUi->nameInput->setText(mClientInfo.getName());
+    mUi->lastNameInput->setText(mClientInfo.getLastName());
+    mUi->phoneInput->setText(mClientInfo.getPhoneNumber());
+    mUi->emailInput->setText(mClientInfo.getEmail());
+
+    const auto date = mClientInfo.getBirthDate().isValid()
+                          ? mClientInfo.getBirthDate()
+                          : QDate::currentDate();
+    mUi->birthdateInput->setDate(date);
+    mUi->ageInput->setText(mClientInfo.getAge());
+
+    mUi->countryInput->setText(mClientInfo.getCountry());
+    mUi->cityInput->setText(mClientInfo.getCity());
+    mUi->timezoneInput->setText(mClientInfo.getTimezone());
+
+    mUi->diagnosisTextEdit->setPlainText(mClientInfo.getDiagnosis());
+    mUi->additionalInfoTextEdit->setPlainText(mClientInfo.getAdditionalInfo());
+}
+
+void QClientInfoCardPage::connectReactiveSignals() {
+    // Updating age with date changed
+    connect(mUi->birthdateInput, &QDateTimeEdit::dateChanged, this, &QClientInfoCardPage::updateAgeInput);
+
+    // Update Avatar when name and last name entered
+    connect(mUi->nameInput, &QLineEdit::textChanged, this, &QClientInfoCardPage::updateAvatar);
+    connect(mUi->lastNameInput, &QLineEdit::textChanged, this, &QClientInfoCardPage::updateAvatar);
+}
+
+void QClientInfoCardPage::connectSignals() {
+    connect(this, &QClientInfoCardPage::provideUpdateUI, this, &QClientInfoCardPage::updateUiProperty);
+    connect(this, &QClientInfoCardPage::provideClearUI, this, &QClientInfoCardPage::clearUI);
+}
+
+void QClientInfoCardPage::updateAgeInput(const QDate &date) const {
+    const int age{countAge(date)};
+    const auto ageStr = QString::number(age);
+    mUi->ageInput->setText(ageStr);
+}
+
+void QClientInfoCardPage::updateAvatar() const {
+    QString initials;
+
+    if (const auto text = mUi->nameInput->text(); !text.isEmpty()) {
+        const auto firstLatter = text[0];
+        initials += firstLatter.toUpper();
+    }
+
+    if (const auto text = mUi->lastNameInput->text(); !text.isEmpty()) {
+        const auto firstLatter = text[0];
+        initials += firstLatter.toUpper();
+    }
+
+
+    mUi->avatarLabel->setText(initials.isEmpty() ? "??" : initials);
+}
+
+
 int QClientInfoCardPage::countAge(const QDate &birthDate) {
-  const QDate now = QDate::currentDate();
-  int age = now.year() - birthDate.year();
-  if (now.month() < birthDate.month() ||
-      (now.month() == birthDate.month() && now.day() < birthDate.day())) {
-    age--;
-  }
-  return age;
+    const QDate now = QDate::currentDate();
+    int age = now.year() - birthDate.year();
+    if (now.month() < birthDate.month() ||
+        (now.month() == birthDate.month() && now.day() < birthDate.day())) {
+        age--;
+    }
+    return age;
+}
+
+void QClientInfoCardPage::clearUI() const {
+    mUi->nameInput->clear();
+    mUi->lastNameInput->clear();
+    mUi->phoneInput->clear();
+    mUi->emailInput->clear();
+    mUi->birthdateInput->setDate(QDate::currentDate());
+    mUi->ageInput->clear();
+    mUi->countryInput->clear();
+    mUi->cityInput->clear();
+    mUi->timezoneInput->clear();
+    mUi->diagnosisTextEdit->clear();
+    mUi->additionalInfoTextEdit->clear();
+    mUi->avatarLabel->setText("??");
 }
--- src/pages/detail_client_info_page/client_info_card.h
+++ src/pages/detail_client_info_page/client_info_card.h
@@ -19,27 +19,40 @@
 #include "qclient.h"
 
 namespace Ui {
-class ClientInfoCard;
+    class ClientInfoCard;
 }
 
 class QClientInfoCardPage final : public QWidget {
-  Q_OBJECT
-
-signals:
-  void provideUpdateUI();
-  void clearUI();
-
-public slots:
-  void setClientInfo(const std::optional<ObxClient> &client);
-  void updateUiProperty();
+    Q_OBJECT
 
 public:
-  explicit QClientInfoCardPage(QWidget *parent = nullptr);
-  ~QClientInfoCardPage() override;
+    explicit QClientInfoCardPage(QWidget *parent = nullptr);
+
+    ~QClientInfoCardPage() override;
+
+signals:
+    void provideUpdateUI();
+
+    void provideClearUI();
+
+public slots:
+    void setClientInfo(const std::optional<ObxClient> &client);
+
+    void updateUiProperty() const;
+
+    void updateAgeInput(const QDate &date) const;
+
+    void updateAvatar() const;
+
+    void clearUI() const;
 
 private:
-  std::unique_ptr<Ui::ClientInfoCard> mUi;
-  QClient mClientInfo;
+    std::unique_ptr<Ui::ClientInfoCard> mUi;
+    QClient mClientInfo;
 
-  static int countAge(const QDate &birthDate);
+    static int countAge(const QDate &birthDate);
+
+    void connectReactiveSignals();
+
+    void connectSignals();
 };
