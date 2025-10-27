291c5c169f6059d26de93a6361939e9da72e2873
Artem Durynin <mail@durynin1.ru>
2025-10-27 23:21:05 +0300

    feat: Added duckdb and remove some import from config.h

--- src/config/config.h
+++ src/config/config.h
@@ -2,11 +2,7 @@
 
 #include <Poco/Path.h>
 #include <rfl/Flatten.hpp>
-#include <rfl/default.hpp>
-#include <rfl/Skip.hpp>
 #include <rfl/yaml.hpp>
-#include <rfl/yaml/load.hpp>
-#include <rfl/yaml/save.hpp>
 #include "rfl_path.hpp"
 
 namespace pcm::config {
