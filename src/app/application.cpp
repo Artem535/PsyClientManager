9a3d34bcccfb42b62f3678e9ea0da143c5f3057a
Artem Durynin <artem5542@gmail.com>
2026-07-30 17:57:59 +0300

    chore: bump version to 0.1.19

--- src/app/application.cpp
+++ src/app/application.cpp
@@ -30,7 +30,7 @@ int Application::run(int argc, char *argv[]) {
   app.setOrganizationName("PsyClientManager");
   app.setApplicationName("PsyClientManager");
   app.setApplicationDisplayName("PsyClientManager");
-  app.setApplicationVersion("0.1.18");
+  app.setApplicationVersion("0.1.19");
   app.setWindowIcon(QIcon(":/icons/brain-solid-full.svg"));
   auto *style = new oclero::qlementine::QlementineStyle(&app);
   app.setStyle(style);
